1c49cea873ce26b0766c64614c6fafa9b6be0861
Gleb Zakharov <webmaster-80@yandex.ru>
2022-11-10 16:38:16 +0300

    feat: добавить интерфейс TaskManager с методами

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-import model.Epic;
-import model.Status;
-import model.Subtask;
-import model.Task;
-
 public class Main {
     public static void main(String[] args) {
 
--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -1,4 +1,50 @@
 package service;
 
+import model.Epic;
+import model.Subtask;
+import model.Task;
+
+import java.util.List;
+
 public interface TaskManager {
+    // Task
+    Task getTask(int id);
+
+    List<Task> getAllTasks();
+
+    void addTask(Task task);
+
+    void updateTask(Task task);
+
+    void deleteTask(int id);
+
+    void deleteAllTasks();
+
+    // Subtask
+    Subtask getSubtask(int id);
+
+    List<Subtask> getAllSubtasks();
+
+    List<Subtask> getSubtasksByEpicId(int epicId);
+
+    void updateSubtask(Subtask subtask);
+
+    void deleteSubtask(int id);
+
+    void deleteAllSubtasks();
+
+    // Epic
+    Epic getEpic(int id);
+
+    public List<Epic> getAllEpics();
+
+    public void addEpic(Epic epic);
+
+    void addSubtaskToEpic(int epicId, String subtaskTitle, String subtaskDescription);
+
+    void updateEpic(Epic epic);
+
+    void deleteEpic(int id);
+
+    void deleteAllEpics();
 }
