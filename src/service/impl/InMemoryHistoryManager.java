53cd28701bd46f17e37c32c5ff2c03d5cc3c75a3
Gleb Zakharov <webmaster-80@yandex.ru>
2022-11-17 14:05:12 +0300

    refactor: добавить константу в InMemoryHistoryManager

--- src/service/impl/InMemoryHistoryManager.java
+++ src/service/impl/InMemoryHistoryManager.java
@@ -7,16 +7,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
+    // максимальное число элементов в истории
+    private static final Integer MAX_ELEMENTS = 10;
     // список истории
     private List<BaseTask> historyList;
 
     public InMemoryHistoryManager() {
-        this.historyList = new ArrayList<>();
+        this.historyList = new ArrayList<>(MAX_ELEMENTS);
     }
 
     @Override
     public void add(BaseTask task) {
-        if (historyList.size() == 10) {
+        if (historyList.size() == MAX_ELEMENTS) {
             historyList.remove(0);
         }
         historyList.add(task);
